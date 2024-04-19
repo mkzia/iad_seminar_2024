2481b208c8bb11c940e452dc975e6e1db636a5f8
Mohammad K. Zia <mkhawarzia@gmail.com>
2024-04-19 07:35:06 -0400

    update

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,18 +1,35 @@
-from typing import Union
 import dill
 from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import numpy as np
 
 app = FastAPI()
 
 with open('rfr_v1.pkl', 'rb') as f: 
-    model = dill.load(f) 
+    reloaded_model = dill.load(f) 
+
+class Payload(BaseModel): 
+    longitude: float
+    latitude: float
+    housing_median_age: float
+    total_rooms: float
+    total_bedrooms: float
+    population: float
+    households: float
+    median_income: float
+    ocean_proximity: str
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/")
+def predict(payload: Payload):
+    df = pd.DataFrame([payload.model_dump().values()], columns=payload.model_dump().keys())
+    df["income_cat"] = pd.cut(df["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+    y_hat = reloaded_model.predict(df)
+    return {"prediction": y_hat[0]}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+
